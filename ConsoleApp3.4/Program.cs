ef7ade3d6f43c1574cc049c8100cbe3b9ec6a57a
Alex Zverev <lordzver007@yandex.ru>
2022-04-04 23:05:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3.4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+
+            int [] array = new int[0];
+            
+            Console.WriteLine(" добавить число, sum - сложить, exit - выход");
+
+            while (isWorking)
+            {               
+                var userInput = Console.ReadLine();
+
+                if (userInput == "exit")
+                {
+                    isWorking = false;
+                    break;
+                }
+                else if (userInput == "sum")
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        sum += array[i];
+                    }
+
+                    Console.WriteLine("Сумма всех чисел в массиве : " + sum);
+
+                    break;
+                }
+                else
+                {
+                    int[] tempArray = new int[array.Length + 1];
+                    int number = Convert.ToInt32(userInput);
+                    
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        tempArray[i] = array[i];
+                    }
+                    tempArray[tempArray.Length - 1] = number;
+                    array = tempArray;
+
+                    for(int i = 0; i < array.Length; i++)
+                    {
+                        Console.Write(array[i]+ " ");                        
+                    }                    
+                }
+            }                
+        }
+    }
+}
